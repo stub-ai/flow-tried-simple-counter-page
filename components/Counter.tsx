7e9d26ed13f7a97f09ec8e02dc642db1ef0153d6
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-11-04 03:54:59 -0700

    Commit from Stub AI (#2)

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/counter')
+      .then(response => response.json())
+      .then(data => setCount(data.value));
+  }, []);
+
+  const increment = () => {
+    fetch('/api/counter', { method: 'POST' })
+      .then(response => response.json())
+      .then(data => setCount(data.value));
+  };
 
   return (
     <div className="flex items-center justify-center space-x-4">
       <button
         className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 active:bg-blue-800 focus:outline-none"
-        onClick={() => setCount(count + 1)}
+        onClick={increment}
       >
         Increase
       </button>
