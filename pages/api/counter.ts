218090b8620004acab41397a901975c322803f35
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-11-04 04:04:34 -0700

    Commit from Stub AI (#5)

--- pages/api/counter.ts
+++ pages/api/counter.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-let counter = 0;
+export let counter = { value: 0 };
 
 type Data = {
   value: number
@@ -10,12 +10,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method === 'POST') {
-    if (req.url && req.url.endsWith('/increment')) {
-      counter++;
-    } else if (req.url && req.url.endsWith('/decrement')) {
-      counter--;
-    }
-  }
-  res.status(200).json({ value: counter })
+  res.status(200).json({ value: counter.value })
 }
